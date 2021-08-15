a6417c7ad16225d090cf5aa1866c064253c51cb5
xkww3n <xkww3n@outlook.com>
2021-08-15 14:23:28 +0800

    Initial commit

---
+++ export.py
@@ -0,0 +1,101 @@
+from ruamel import yaml
+import sys
+import shutil
+import os
+import copy
+import subprocess
+
+exporter_cfg_path = sys.argv[1]
+exporter_cfg = open(exporter_cfg_path,mode='r',encoding='utf-8')
+loadedExporterConfig = yaml.load(exporter_cfg,Loader=yaml.RoundTripLoader)
+
+configPath = loadedExporterConfig['mkdocs_config']
+softwarePath = loadedExporterConfig['generated_software_page']
+operationsPath = loadedExporterConfig['generated_operations_page']
+software_imgs = "./tech-guide/docs/daily-maintenance/software/images"
+operations_imgs = "./tech-guide/docs/daily-maintenance/operations/images"
+
+config = open(configPath,mode='r',encoding='utf-8')
+software = open(softwarePath,mode='w',encoding='utf-8')
+operations = open(operationsPath,mode='w',encoding='utf-8')
+loadedCfg = yaml.load(config,Loader=yaml.RoundTripLoader)
+newCfg = copy.deepcopy(loadedCfg)
+tmp_cfg = open("./tech-guide/tmp.yml",mode='w',encoding='utf-8')
+
+for i in range(len(loadedCfg['nav'])):
+    tmp = list(loadedCfg['nav'][i].values())[0]
+    for j in range(len(tmp)):
+        if type(list(tmp[j].values())[0]) == yaml.comments.CommentedSeq and str(list(tmp[j].keys())[0]) == "软件篇":
+            for k in range(len(list(tmp[j].values())[0])):
+                path = "./tech-guide/docs/"+str(list(list(tmp[j].values())[0][k].values())[0])
+                print(path," has been merged to ",softwarePath)
+                file = open(path,mode='r',encoding='utf-8')
+                software.write("# 软件篇\n")
+                software.write(file.read())
+                file.close()
+
+        if type(list(tmp[j].values())[0]) == yaml.comments.CommentedSeq and str(list(tmp[j].keys())[0]) == "操作篇":
+            for k in range(len(list(tmp[j].values())[0])):
+                path = "./tech-guide/docs/"+str(list(list(tmp[j].values())[0][k].values())[0])
+                print(path," has been merged to ",operationsPath)
+                file = open(path,mode='r',encoding='utf-8')
+                operations.write("# 操作篇\n")
+                operations.write(file.read())
+                file.close()
+
+newCfg['nav'][1]['日常维护'][0]['软件篇'] = "daily-maintenance/software.md"
+newCfg['nav'][1]['日常维护'][1]['操作篇'] = "daily-maintenance/operations.md"
+newCfg['copyright'] = "本手册所有内容均在 CC BY-SA 4.0 和 SATA 协议条款下提供。"
+newCfg['markdown_extensions'][7]['toc'] = {'permalink': 'true'}
+
+if os.getenv('ERROR_DEBUG') == "1":
+    newCfg['markdown_extensions'][6] = "114514"
+
+del newCfg['edit_uri']
+del newCfg['repo_url']
+del newCfg['repo_name']
+newCfg['plugins'] = [{'mkpdfs': {'design': '../mkpdfs-style/report.css', 'company': '赣州三中学生会', 'author': '秘书处', 'toc_title': '目录'}}]
+
+yaml.dump(newCfg,tmp_cfg,Dumper=yaml.RoundTripDumper,default_flow_style=False,encoding='utf-8',allow_unicode=True)
+operations.close()
+software.close()
+config.close()
+tmp_cfg.close()
+os.remove(configPath)
+os.remove("./tech-guide/docs/landing.md")
+os.rename("./tech-guide/tmp.yml",configPath)
+print("New mkdocs configuration generated.")
+
+software_imgs_list=os.listdir(software_imgs)
+operations_imgs_list=os.listdir(operations_imgs)
+
+if not os.path.exists("./tech-guide/docs/daily-maintenance/images/"):
+    os.mkdir("./tech-guide/docs/daily-maintenance/images/")
+
+for i in range(len(os.listdir(software_imgs))):
+    path=os.path.join(software_imgs,software_imgs_list[i])
+    src=os.path.join("./tech-guide/docs/daily-maintenance/software/images",software_imgs_list[i])
+    dst="./tech-guide/docs/daily-maintenance/images/"
+    shutil.move(src,dst)
+
+for i in range(len(os.listdir(operations_imgs))):
+    path=os.path.join(operations_imgs,operations_imgs_list[i])
+    src=os.path.join("./tech-guide/docs/daily-maintenance/operations/images",operations_imgs_list[i])
+    dst="./tech-guide/docs/daily-maintenance/images/"
+    shutil.move(src,dst)
+
+print("Images have been merged.\nCleaning up...")
+shutil.rmtree("./tech-guide/docs/daily-maintenance/operations")
+shutil.rmtree("./tech-guide/docs/daily-maintenance/software")
+
+print("Generating PDF...")
+try:
+    subprocess.check_call("cd tech-guide && mkdocs build",shell=True)
+
+except subprocess.CalledProcessError:
+    print("Generation failed. Check the output above for more information.")
+
+else:
+    shutil.move("./tech-guide/site/pdf/combined.pdf","./result.pdf")
+    shutil.rmtree("./tech-guide/site")
+    print("All done. Get the generated PDF at ./result.pdf")
